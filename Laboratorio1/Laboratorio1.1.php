2d1d1bdc154a9c32cee068e94b6ed10e383c33ae
Pdeleon24 <pau.yarit00@gmail.com>
2022-09-07 18:23:20 -0500

    labs

---
+++ Laboratorio1/Laboratorio1.1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laboratorio 1.1</title>
+</head>
+<body>
+    <?php
+    $ini = "Hola ";
+    $fin = " a todos";
+    $todo = $ini.$fin;
+    echo $todo;
+    ?> 
+</body>
+</html>
