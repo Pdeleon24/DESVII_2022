d652f250f5843a017ac237f8f7d8b16e9ee3abb4
Pdeleon24 <pau.yarit00@gmail.com>
2022-11-02 19:24:04 -0500

    Labs 17

---
+++ api/objetos/producto.php
@@ -0,0 +1,121 @@
+<?php
+class Producto{
+    private $conn;
+    private $nombre_tabla = "productos";
+
+    public $id;
+    public $nombre;
+    public $descripcion;
+    public $precio;
+    public $categoria_id;
+    public $categoria_desc;
+    public $creado;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    function read(){
+        $query = "SELECT
+        c.nombre as categoria_desc, p.id, p.nombre, p.descripcion, p.precio, p.categoria_id, p.creado
+        FROM 
+        " . $this->nombre_tabla . " p
+        LEFT JOIN
+        categorias c
+        ON p.categoria_id = c.id
+        ORDER BY
+        p.creado DESC";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    function crear() {
+
+        $query = "INSERT INTO " . $this->nombre_tabla . " SET nombre=:nombre, precio=:precio, descripcion=:descripcion, categoria_id=:categoria_id, creado=:creado";
+       
+        $stmt = $this->conn->prepare( $query );
+        
+        $this->nombre = htmlspecialchars( strip_tags( $this->nombre ) );
+        $this->precio = htmlspecialchars( strip_tags( $this->precio ) );
+        $this->descripcion = htmlspecialchars( strip_tags( $this->descripcion ) );
+        $this->categoria_id = htmlspecialchars( strip_tags( $this->categoria_id ) );
+        $this->creado = htmlspecialchars( strip_tags( $this->creado ) );
+        
+        $stmt->bindParam( ":nombre", $this->nombre );
+        $stmt->bindParam( ":precio", $this->precio );
+        $stmt->bindParam( ":descripcion", $this->descripcion );
+        $stmt->bindParam( ":categoria_id", $this->categoria_id );
+        $stmt->bindParam( ":creado", $this->creado );
+        
+        if ( $stmt->execute() ) {
+            return true;
+        }
+        return false;
+    }
+
+    function readOne() {
+
+        $query = "SELECT c.nombre as categoria_desc, p.id, p.nombre, p.descripcion, p.precio, p.categoria_id, p.creado FROM " . $this->nombre_tabla . " p LEFT JOIN categorias c ON p.categoria_id = c.id WHERE p.id = ? LIMIT 0,1";
+        
+        $stmt = $this->conn->prepare( $query );
+        
+        $stmt->bindParam( 1, $this->id );
+        
+        $stmt->execute();
+      
+        $row = $stmt->fetch( PDO::FETCH_ASSOC );
+        
+        $this->nombre = $row[ 'nombre' ];
+        $this->precio = $row[ 'precio' ];
+        $this->descripcion = $row[ 'descripcion' ];
+        $this->categoria_id = $row[ 'categoria_id' ];
+        $this->categoria_desc = $row[ 'categoria_desc' ];
+    }
+
+    
+    function actualizar() {
+       
+        $query = "UPDATE " . $this->nombre_tabla . " SET nombre=:nombre, precio=:precio, descripcion=:descripcion, categoria_id=:categoria_id WHERE id=:id";
+        
+        $stmt = $this->conn->prepare( $query );
+        
+        $this->nombre = htmlspecialchars( strip_tags( $this->nombre ) );
+        $this->precio = htmlspecialchars( strip_tags( $this->precio ) );
+        $this->descripcion = htmlspecialchars( strip_tags( $this->descripcion ) );
+        $this->categoria_id = htmlspecialchars( strip_tags( $this->categoria_id ) );
+        $this->id = htmlspecialchars( strip_tags( $this->id ) );
+        
+        $stmt->bindParam( ":nombre", $this->nombre );
+        $stmt->bindParam( ":precio", $this->precio );
+        $stmt->bindParam( ":descripcion", $this->descripcion );
+        $stmt->bindParam( ":categoria_id", $this->categoria_id );
+        $stmt->bindParam( ":id", $this->id );
+        
+        if ( $stmt->execute() ) {
+            return true;
+        }
+        return false;
+    }
+
+    
+    function eliminar() {
+       
+        $query = "DELETE FROM " . $this->nombre_tabla . " WHERE id = ?";
+        
+        $stmt = $this->conn->prepare( $query );
+        
+        $this->id = htmlspecialchars( strip_tags( $this->id ) );
+        
+        $stmt->bindParam( 1, $this->id );
+        
+        if ( $stmt->execute() ) {
+            return true;
+        }
+        return false;
+    }
+}
+?>
